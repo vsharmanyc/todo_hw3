281217742c72fd06c2970f89b19cdccb57cbdcaf
vsharmanyc <vsharma.nyc@gmail.com>
2019-11-17 18:09:10 -0500

    move up down delete

--- src/components/list_screen/ItemCard.js
+++ src/components/list_screen/ItemCard.js
@@ -1,26 +1,86 @@
 import React from 'react';
+import { Modal, Button } from 'react-materialize';
+import { getFirestore } from 'redux-firestore';
 
 class ItemCard extends React.Component {
+    moveDown = (e) => {
+        console.log("MOVE DOWN");
+        const fireStore = getFirestore();
+        const { item } = this.props;
+        let key = item["key"];
+        let items = this.props.todoList.items;
+        if (key != items.length - 1) {
+            let temp = items[key + 1];
+            items[key + 1] = items[key]
+            items[key] = temp;
+            items[key + 1]["key"] += 1;
+            items[key]["key"] -= 1;
+
+            fireStore.collection('todoLists').doc(this.props.todoList.id).update({ items: items });
+        }
+    }
+
+    moveUp = (e) => {
+        console.log("MOVE UP");
+        const fireStore = getFirestore();
+        const { item } = this.props;
+        let key = item["key"];
+        if (key != 0) {
+            let items = this.props.todoList.items;
+            let temp = items[key - 1];
+            items[key - 1] = items[key]
+            items[key] = temp;
+            items[key - 1]["key"] -= 1;
+            items[key]["key"] += 1;
+
+            fireStore.collection('todoLists').doc(this.props.todoList.id).update({ items: items });
+        }
+    }
+
+    deleteItem = (e) => {
+        console.log("DELETE ITEM");
+        const fireStore = getFirestore();
+        const { item } = this.props;
+        let key = item["key"];
+        let items = this.props.todoList.items;
+        items.splice(key, 1);
+        for (var i = 0; i < items.length; i++)
+            items[i]["key"] = i;
+
+        fireStore.collection('todoLists').doc(this.props.todoList.id).update({ items: items });
+    }
+
     render() {
-        const { item } = this.props;  
+        const { item } = this.props;
+        console.log("FDFDFDFDFDFDF");
+
         return (
             <div className="card z-depth-0 todo-list-link pink-lighten-3">
                 <div className="card-content grey-text text-darken-3">
                     <div class="row">
-                        <div class="col s4">
-                        <span className="card-title">{item.description}</span>
-                        <span className="card-title">{"Assigned To: " + item.assigned_to}</span>
+                        <div class="col s3">
+                            <span className="card-title">{item.description}</span>
+                            <span className="card-title">{"Assigned To: " + item.assigned_to}</span>
                         </div>
                         <div class="col s3">
-                        <span className="card-title">{item.due_date}</span>
+                            <span className="card-title">{item.due_date}</span>
                         </div>
                         <div class="col s3">
-                        <span className="card-title">{item.completed ? "Completed" : "Pending"}</span>
+                            <span className="card-title">{item.completed ? "Completed" : "Pending"}</span>
                         </div>
-                        
-                    
-                    
-                    
+                        <Button
+                            floating
+                            fab={{
+                                direction: 'left',
+                                hoverEnabled: true,
+                            }}
+                            className="red"
+                            large
+                        >
+                            <Button floating icon={<i class="material-icons">arrow_upward</i>} className="red" onClick={this.moveUp} />
+                            <Button floating icon={<i class="material-icons">arrow_downward</i>} className="yellow darken-1" onClick={this.moveDown} />
+                            <Button floating icon={<i class="material-icons">cancel</i>} className="green" onClick={this.deleteItem} />
+                        </Button>
                     </div>
                 </div>
             </div>
--- src/components/list_screen/ListScreen.js
+++ src/components/list_screen/ListScreen.js
@@ -17,10 +17,10 @@ class ListScreen extends Component {
         const { target } = e;
         console.log(e.target.value);
         const fireStore = getFirestore();
-        if(target.id == "name")
-            fireStore.collection('todoLists').doc(this.props.todoList.id).update({name: target.value});
-        else if(target.id == "owner")
-            fireStore.collection('todoLists').doc(this.props.todoList.id).update({owner: target.value});
+        if (target.id == "name")
+            fireStore.collection('todoLists').doc(this.props.todoList.id).update({ name: target.value });
+        else if (target.id == "owner")
+            fireStore.collection('todoLists').doc(this.props.todoList.id).update({ owner: target.value });
 
         this.setState(state => ({
             ...state,
@@ -28,14 +28,20 @@ class ListScreen extends Component {
         }));
     }
 
+    trashClicked = (e) => {
+        console.log("TRASH CLICKED");
+        const fireStore = getFirestore();
+        var todoListRef = fireStore.collection('todoLists').doc(this.props.todoList.id).delete();
+    }
+
     makeTableHTML(todoList) {
         var result = "<table class=\"table table-striped\"><thead class=\"thead-default\">"
-        +"<tr><th>Task</th><th>Due Date</th><th>Status</th></tr></thead><tbody>";
+            + "<tr><th>Task</th><th>Due Date</th><th>Status</th></tr></thead><tbody>";
         console.log(todoList.items);
-        for(var i=0; i<todoList.items.length; i++) {
-            result += "<tr><td>" + todoList.items[i]["description"] + "<br>Assigned To: " + todoList.items[i]["assigned_to"] +"</br></td>";
-            result += "<td>" + todoList.items[i]["due_date"] +"</td>";
-            result += "<td>" + (todoList.items[i]["completed"] ? "Completed" : "Pending") +"</td></tr>";
+        for (var i = 0; i < todoList.items.length; i++) {
+            result += "<tr><td>" + todoList.items[i]["description"] + "<br>Assigned To: " + todoList.items[i]["assigned_to"] + "</br></td>";
+            result += "<td>" + todoList.items[i]["due_date"] + "</td>";
+            result += "<td>" + (todoList.items[i]["completed"] ? "Completed" : "Pending") + "</td></tr>";
         }
         result += "</tbody></table>";
         return result;
@@ -44,12 +50,14 @@ class ListScreen extends Component {
     render() {
         const auth = this.props.auth;
         const todoList = this.props.todoList;
+        
         if (!auth.uid) {
             return <Redirect to="/" />;
         }
 
         return (
             <div className="container white">
+                <a class="btn-floating btn-small black right"><i class="material-icons" onClick={this.trashClicked} >delete</i></a>
                 <h5 className="grey-text text-darken-3">Todo List</h5>
                 <div className="input-field">
                     <label className="active" htmlFor="email">Name</label>
@@ -59,29 +67,33 @@ class ListScreen extends Component {
                     <label className="active" htmlFor="password">Owner</label>
                     <input type="text" name="owner" id="owner" onChange={this.handleChange} defaultValue={todoList.owner} />
                 </div>
-                
+
                 <ItemsList todoList={todoList} />
-                {/*<div dangerouslySetInnerHTML={{__html: this.makeTableHTML(todoList)}} />*/}
+                {/* this code puts items into a table 
+                <div dangerouslySetInnerHTML={{__html: this.makeTableHTML(todoList)}} />
+                */}
+                <center><a class="btn-floating btn-large black"><i class="material-icons">add_circle</i></a></center>
+                <br></br>
             </div>
         );
     }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const { id } = ownProps.match.params;
-  const { todoLists } = state.firestore.data;
-  const todoList = todoLists ? todoLists[id] : null;
-  todoList.id = id;
+    const { id } = ownProps.match.params;
+    const { todoLists } = state.firestore.data;
+    const todoList = todoLists ? todoLists[id] : null;
+    todoList.id = id;
 
-  return {
-    todoList,
-    auth: state.firebase.auth,
-  };
+    return {
+        todoList,
+        auth: state.firebase.auth,
+    };
 };
 
 export default compose(
-  connect(mapStateToProps),
-  firestoreConnect([
-    { collection: 'todoLists' },
-  ]),
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'todoLists' },
+    ]),
 )(ListScreen);
